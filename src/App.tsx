559e1d89334a83bd91a473c879eab753f4e55036
Kaan Yılmaz <kaan.yilmaz.21588@ozu.edu.tr>
2022-07-21 01:37:33 +0300

    sorting now works as expected

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,40 @@ import CategorySelection from "./components/CategorySelection";
 import ProductList from "./components/ProductList";
 import CartFab from "./components/CartFab";
 import {useDispatch, useSelector} from "react-redux";
-import productsSlice, {setProducts, setDefaultProducts} from "./features/Products/productsSlice";
+import productsSlice, {setProducts, setDefaultProducts, sortProducts} from "./features/Products/productsSlice";
 
 import axios from "axios";
 import SortingComponent from "./components/SortingComponent";
+import {setSorting} from "./features/Products/sortingSlice";
 
 const baseURL = "https://fakestoreapi.com/products";
 
 function App() {
     const dispatch = useDispatch();
+    const sorting = useSelector((state: any) => state.sorting.sorting);
 
     axios.get(`${baseURL}`).then((response) => {
-        dispatch(setProducts(response.data))
+        dispatch(setProducts(response.data));
+        handleSorting();
     });
 
 
     axios.get(`${baseURL}`).then((response) => {
         dispatch(setDefaultProducts(response.data))
     });
+
+    const handleSorting = () => {
+        let value0 = sorting;
+        if (value0 === "MostPrice") {
+            //sort from the most points
+            dispatch(sortProducts((function (a: any, b: any) { return b.price - a.price })));
+        } else if (value0 === "LeastPrice") {
+            console.log("least")
+            //sort from the least points
+            dispatch(sortProducts((function (a: any, b: any) { return a.price - b.price })));
+        }
+        dispatch(setSorting(value0));
+    };
 
 
     return (
