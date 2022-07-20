cc3652fa64b49e62992e6235275117b0047db10f
Kaan Yılmaz <kaan.yilmaz.21588@ozu.edu.tr>
2022-07-21 02:10:24 +0300

    Revert "sorting now works as expected"
    This reverts commit 559e1d89334a83bd91a473c879eab753f4e55036.

--- src/components/CategorySelection.tsx
+++ src/components/CategorySelection.tsx
@@ -5,20 +5,13 @@ import Stack from '@mui/material/Stack';
 import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 import {useDispatch, useSelector} from "react-redux";
-import productsSlice, {
-    filterProducts,
-    resetProducts,
-    setProducts,
-    sortProducts
-} from "../features/Products/productsSlice";
+import productsSlice, {filterProducts, resetProducts, setProducts} from "../features/Products/productsSlice";
 import {setCategory} from "../features/Products/filterSlice";
-import {setSorting} from "../features/Products/sortingSlice";
 
 
 function CategorySelection(props: any) {
     const dispatch = useDispatch();
     const products = useSelector((state: any) => state.products.productsArray);
-    const sorting = useSelector((state: any) => state.sorting.sorting);
 
     function handleClick(e: any) {
         e.preventDefault();
@@ -30,24 +23,8 @@ function CategorySelection(props: any) {
         }
         dispatch(setCategory(ToFilter));
         dispatch(filterProducts(ToFilter));
-        handleSorting();
         console.log(products);
     }
-
-    const handleSorting = () => {
-        let value0 = sorting;
-        if (value0 === "MostPrice") {
-            //sort from the most points
-            dispatch(sortProducts((function (a: any, b: any) { return b.price - a.price })));
-        } else if (value0 === "LeastPrice") {
-            console.log("least")
-            //sort from the least points
-            dispatch(sortProducts((function (a: any, b: any) { return a.price - b.price })));
-        }
-        dispatch(setSorting(value0));
-    };
-
-
     return(
         <Grid container display="flex" justifyContent="center">
             <Grid item xs={12} display="flex">
