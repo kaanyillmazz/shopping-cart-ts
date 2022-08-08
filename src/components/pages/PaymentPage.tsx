3a82014bb9e94f0c08cfa1e00e7ffd802478521b
kyilmaz <kaan.yilmaz.21588@ozu.edu.tr>
2022-08-08 10:16:42 +0300

    added form control to payment

--- src/components/pages/PaymentPage.tsx
+++ src/components/pages/PaymentPage.tsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 
 
 import "./PaymentPage.css";
-import {Button, Grid, TextField} from "@mui/material";
+import {Button, Grid, Input, TextField} from "@mui/material";
 import {useDispatch, useSelector} from "react-redux";
 import {setShippingDisplay} from "../../features/Products/checkoutSlice";
 
@@ -17,23 +17,41 @@ function PaymentPage(props: any) {
     const [cardCCV, setCardCCV] = useState("");
 
 
-
     const handleSubmit = (event: any) => {
-
+        event.preventDefault();
+        dispatch(setShippingDisplay("flex"));
+        document.getElementById("paymentFormContainer")!.classList.add("drop");
     }
     const handleChange = (event: any) => {
+        let length = event.target.value.length;
+        let value = event.target.value;
         switch (event.target.id) {
             case "cardHolderName":
-                setNameField(event.target.value);
+                setNameField(value);
                 break;
             case "cardNumber":
-                setCardNumber(event.target.value)
+                if (event.keyCode === 8) {
+                    setCardNumber(cardNumber.slice(0, -1));
+                    break;
+                } else if (length === 19) {
+                    break;
+                } else if (event.keyCode < 48 || event.keyCode > 57) {
+                    break;
+                } else if (length === 4 || length === 9 || length === 14) {
+                    setCardNumber(value + "-");
+                    break;
+                } else {
+                    setCardNumber(cardNumber + event.key);
+                }
                 break;
             case "cardExp":
-                setCardExp(event.target.value)
+                setCardExp(value)
                 break;
             case "cardCCV":
-                setCardCCV(event.target.value)
+                if (length === 4) {
+                    break;
+                }
+                setCardCCV(value)
                 break;
             default:
         }
@@ -41,55 +59,39 @@ function PaymentPage(props: any) {
     }
 
 
-    return (<Grid container className="paymentFormContainer animate" id="paymentFormContainer" display={display}>
-        <Grid>
-            <form onSubmit={handleSubmit}>
-                <Grid item xs={12}>
-                    <label>CardHolder Name</label>
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField className="myLabel" id="cardHolderName" label="Name Surname" variant="standard"
-                               value={nameField}
-                               onChange={handleChange}/>
-                </Grid>
-                <Grid item xs={12}>
-                    <label>Card Number</label>
-                </Grid>
+    return (
+        <Grid container className="paymentFormContainer animate" id="paymentFormContainer" display={display}>
+            <Grid>
+                <form onSubmit={handleSubmit}>
+                    <Grid item xs={12}>
+                        <TextField required={true} type="text" className="myLabel" id="cardHolderName"
+                                   label="Cardholder Name"
+                                   variant="standard"
+                                   value={nameField}
+                                   onChange={handleChange}/>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField onKeyDown={handleChange} type="tel" required={true} className="myLabel"
+                                   id="cardNumber"
+                                   label="Card Number" variant="standard"
+                                   value={cardNumber}
+                        />
+                    </Grid>
+                    <Grid item xs={5}>
+                        <TextField required={true} type="month" className="myLabel" id="cardExp" label="Exp Date"
+                                   variant="standard" value={cardExp}
+                                   onChange={handleChange}/>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <TextField required={true} type="tel" className="myLabel" id="cardCCV" label="CCV"
+                                   variant="standard" value={cardCCV}
+                                   onChange={handleChange}/>
+                    </Grid>
 
-                <Grid item xs={12}>
-                    <TextField className="myLabel" id="cardNumber" label="xxx xxxx xxxx xxxx" variant="standard"
-                               value={cardNumber}
-                               onChange={handleChange}/>
-                </Grid>
-
-                <Grid item xs={5}>
-                    <label>Exp Date</label>
-                </Grid>
-
-                <Grid item xs={5}>
-                    <TextField className="myLabel" id="cardExp" label="Exp Date" variant="standard" value={cardExp}
-                               onChange={handleChange}/>
-                </Grid>
-
-                <Grid item xs={3}>
-                    <label>ccv</label>
-                </Grid>
-
-                <Grid item xs={3}>
-                    <TextField className="myLabel" id="cardCCV" label="CCV" variant="standard" value={cardCCV}
-                               onChange={handleChange}/>
-                </Grid>
-            </form>
-
-        </Grid>
-
-        <Grid xs={12}>
-            <Button variant={"contained"} onClick={()=>{dispatch(setShippingDisplay("flex"));
-                document.getElementById("paymentFormContainer")!.classList.add("drop");
-            }}>Proceed</Button>
-        </Grid>
-
-    </Grid>);
+                    <Input type="submit" value="Proceed"></Input>
+                </form>
+            </Grid>
+        </Grid>);
 }
 
 export default PaymentPage;
